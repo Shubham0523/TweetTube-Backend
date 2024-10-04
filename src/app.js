5edded104bd58970be333d50de9c69810b5cb571
Shubham0523 <shubhamarora2306@gmail.com>
2024-10-04 15:27:30 +0530

    add frontend origin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import morgan from 'morgan';
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://tweet-tube-frontend.vercel.app/"],
     credentials: true,
 }))
 
